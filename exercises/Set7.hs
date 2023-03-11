d6d28d7820258a03ddfe479fd3fe3c505c04a950
Shuze <liushuze_lsz@hotmail.com>
2023-03-12 00:32:06 +0100

    Part1 finished!

--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -4,7 +4,7 @@ module Set7 where
 
 import Mooc.Todo
 import Data.List
-import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.List.NonEmpty (NonEmpty ((:|)), insert)
 import Data.Monoid
 import Data.Semigroup
 
@@ -26,11 +26,11 @@ data Velocity = Velocity Double
 
 -- velocity computes a velocity given a distance and a time
 velocity :: Distance -> Time -> Velocity
-velocity = todo
+velocity (Distance d) (Time t) = Velocity (d / t)
 
 -- travel computes a distance given a velocity and a time
 travel :: Velocity -> Time -> Distance
-travel = todo
+travel (Velocity v) (Time t) = Distance (v * t)
 
 ------------------------------------------------------------------------------
 -- Ex 2: let's implement a simple Set datatype. A Set is a list of
@@ -46,18 +46,26 @@ travel = todo
 
 data Set a = Set [a]
   deriving (Show,Eq)
-
+-- ! data type /= matching pattern!
 -- emptySet is a set with no elements
 emptySet :: Set a
-emptySet = todo
+emptySet = Set []
 
 -- member tests if an element is in a set
 member :: Eq a => a -> Set a -> Bool
-member = todo
+member x (Set (s:ss)) = x == s || member x (Set ss)
+member x emptySet = False
+
+listOfSet :: Set a -> [a]
+listOfSet (Set s) = s
 
 -- add a member to a set
-add :: a -> Set a -> Set a
-add = todo
+add :: (Ord a) => a -> Set a -> Set a
+add e (Set (s:ss))
+  | s == e = Set (s:ss)
+  | s > e = Set (e:s:ss)
+  | s < e = Set (s:listOfSet (add e (Set ss)))
+add e emptySet = Set [e]
 
 ------------------------------------------------------------------------------
 -- Ex 3: a state machine for baking a cake. The type Event represents
@@ -92,10 +100,26 @@ add = todo
 data Event = AddEggs | AddFlour | AddSugar | Mix | Bake
   deriving (Eq,Show)
 
-data State = Start | Error | Finished
+data State = Start | Error | Finished | EggAdded | FlourAdded | SugarAdded | FlourASugarAdded | Mixed
   deriving (Eq,Show)
 
-step = todo
+step :: State -> Event -> State
+step Error _ = Error
+step Finished _ = Finished
+
+step Start AddEggs = EggAdded
+step Start _ = Error
+step EggAdded AddFlour = FlourAdded
+step EggAdded AddSugar = SugarAdded
+step EggAdded _ = Error
+step FlourAdded AddSugar = FlourASugarAdded
+step FlourAdded _ = Error
+step SugarAdded AddFlour = FlourASugarAdded
+step SugarAdded _ = Error
+step FlourASugarAdded Mix = Mixed
+step FlourASugarAdded _ = Error
+step Mixed Bake = Finished
+step Mixed _ = Error
 
 -- do not edit this
 bake :: [Event] -> State
@@ -115,15 +139,20 @@ bake events = go Start events
 --   average (1.0 :| [2.0,3.0])  ==>  2.0
 
 average :: Fractional a => NonEmpty a -> a
-average = todo
+average xs = sum xs / fromIntegral (length xs)
 
 ------------------------------------------------------------------------------
 -- Ex 5: reverse a NonEmpty list.
 --
 -- PS. The Data.List.NonEmpty type has been imported for you
 
+toList :: NonEmpty a -> [a]
+toList (x:|xs) = x:xs
+
 reverseNonEmpty :: NonEmpty a -> NonEmpty a
-reverseNonEmpty = todo
+reverseNonEmpty xs = 
+  case reverse (toList xs) of
+    (a:as) -> a:|as
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement Semigroup instances for the Distance, Time and
@@ -134,8 +163,14 @@ reverseNonEmpty = todo
 --
 -- velocity (Distance 50 <> Distance 10) (Time 1 <> Time 2)
 --    ==> Velocity 20
+instance Semigroup Distance where
+  (<>) (Distance d1) (Distance d2) = Distance (d1 + d2)
 
+instance Semigroup Time where
+  (<>) (Time d1) (Time d2) = Time (d1 + d2)
 
+instance Semigroup Velocity where
+  (<>) (Velocity d1) (Velocity d2) = Velocity (d1 + d2)
 ------------------------------------------------------------------------------
 -- Ex 7: implement a Monoid instance for the Set type from exercise 2.
 -- The (<>) operation should be the union of sets.
@@ -143,7 +178,18 @@ reverseNonEmpty = todo
 -- What's the right definition for mempty?
 --
 -- What are the class constraints for the instances?
+instance (Ord a) => Semigroup (Set a) where
+  (Set s1) <> (Set s2) = 
+    let 
+      addList :: (Ord a) => [a] -> Set a -> Set a
+      addList [] (Set x) = Set x
+      addList (x:xs) (Set s) = addList xs (add x (Set s))
+    in
+      addList s1 (Set s2)
 
+
+instance (Ord a) => Monoid (Set a) where
+  mempty = Set []
 
 ------------------------------------------------------------------------------
 -- Ex 8: below you'll find two different ways of representing
@@ -166,29 +212,42 @@ reverseNonEmpty = todo
 
 data Operation1 = Add1 Int Int
                 | Subtract1 Int Int
+                | Multiply1 Int Int
   deriving Show
 
 compute1 :: Operation1 -> Int
 compute1 (Add1 i j) = i+j
 compute1 (Subtract1 i j) = i-j
+compute1 (Multiply1 i j) = i*j
 
 show1 :: Operation1 -> String
-show1 = todo
+show1 (Add1 a b) = show a ++ "+" ++ show b 
+show1 (Subtract1 a b) = show a ++ "-" ++ show b 
+show1 (Multiply1 a b) = show a ++ "*" ++ show b 
 
 data Add2 = Add2 Int Int
   deriving Show
 data Subtract2 = Subtract2 Int Int
   deriving Show
 
+data Multiply2 = Multiply2 Int Int
+  deriving Show
+
 class Operation2 op where
   compute2 :: op -> Int
+  show2 :: op -> String
 
 instance Operation2 Add2 where
   compute2 (Add2 i j) = i+j
+  show2 (Add2 a b) = show a ++ "+" ++ show b 
 
 instance Operation2 Subtract2 where
   compute2 (Subtract2 i j) = i-j
+  show2 (Subtract2 a b) = show a ++ "-" ++ show b 
 
+instance Operation2 Multiply2 where
+  compute2 (Multiply2 i j) = i*j
+  show2 (Multiply2 a b) = show a ++ "*" ++ show b 
 
 ------------------------------------------------------------------------------
 -- Ex 9: validating passwords. Below you'll find a type
@@ -217,7 +276,13 @@ data PasswordRequirement =
   deriving Show
 
 passwordAllowed :: String -> PasswordRequirement -> Bool
-passwordAllowed = todo
+passwordAllowed str (MinimumLength c) = length str >= c
+passwordAllowed str (ContainsSome []) = False
+passwordAllowed str (ContainsSome (x:xs)) = elem x str || passwordAllowed str (ContainsSome xs)
+passwordAllowed str (DoesNotContain []) = True
+passwordAllowed str (DoesNotContain (x:xs)) = notElem x str && passwordAllowed str (DoesNotContain xs)
+passwordAllowed str (And req1 req2) = passwordAllowed str req1 && passwordAllowed str req2
+passwordAllowed str (Or req1 req2) = passwordAllowed str req1 || passwordAllowed str req2
 
 ------------------------------------------------------------------------------
 -- Ex 10: a DSL for simple arithmetic expressions with addition and
@@ -239,17 +304,21 @@ passwordAllowed = todo
 --     ==> "(3*(1+1))"
 --
 
-data Arithmetic = Todo
+data Arithmetic = Literal Integer | Operation String Arithmetic Arithmetic
   deriving Show
 
 literal :: Integer -> Arithmetic
-literal = todo
+literal = Literal
 
 operation :: String -> Arithmetic -> Arithmetic -> Arithmetic
-operation = todo
+operation str a1 a2 = Operation str a1 a2
 
 evaluate :: Arithmetic -> Integer
-evaluate = todo
+evaluate (Literal x) = x
+evaluate (Operation "+" a1 a2) = evaluate a1 + evaluate a2
+evaluate (Operation "*" a1 a2) = evaluate a1 * evaluate a2
 
 render :: Arithmetic -> String
-render = todo
+render (Literal x) = show x
+render (Operation "+" a1 a2) = "(" ++ render a1 ++ "+" ++ render a2 ++ ")"
+render (Operation "*" a1 a2) = "(" ++ render a1 ++ "*" ++ render a2 ++ ")"
--- exercises/Set8.hs
+++ exercises/Set8.hs
@@ -133,7 +133,13 @@ renderListExample = renderList justADot (9,11) (9,11)
 --      ["000000","000000","000000"]]
 
 dotAndLine :: Picture
-dotAndLine = todo
+dotAndLine = Picture f
+  where 
+    f :: Coord -> Color
+    f (Coord 3 4) = white
+    f (Coord _ 8) = pink
+    f (Coord _ _) = black
+
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
@@ -165,11 +171,15 @@ dotAndLine = todo
 --          ["7f0000","ff7f7f","7f0000"],
 --          ["7f0000","7f0000","7f0000"]]
 
+avg :: Int -> Int -> Int
+avg x y = (x + y) `div` 2
+
 blendColor :: Color -> Color -> Color
-blendColor = todo
+blendColor (Color r1 g1 b1) (Color r2 g2 b2) = Color (avg r1 r2) (avg g1 g2) (avg b1 b2)
 
 combine :: (Color -> Color -> Color) -> Picture -> Picture -> Picture
-combine = todo
+-- coord -> Color
+combine f (Picture f1) (Picture f2) = Picture (\x -> f (f1 x) (f2 x))
 
 ------------------------------------------------------------------------------
 
@@ -230,7 +240,11 @@ exampleCircle = fill red (circle 80 100 200)
 --        ["000000","000000","000000","000000","000000","000000"]]
 
 rectangle :: Int -> Int -> Int -> Int -> Shape
-rectangle x0 y0 w h = todo
+rectangle x0 y0 w h = Shape f
+  where
+    f :: Coord -> Bool
+    f (Coord x y) = x >= x0 && x < (x0 + w) && y >= y0 && y < (y0 + h)
+
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
@@ -246,10 +260,16 @@ rectangle x0 y0 w h = todo
 -- shape.
 
 union :: Shape -> Shape -> Shape
-union = todo
+union (Shape f1) (Shape f2) = Shape f
+  where
+    f :: Coord -> Bool
+    f coord = f1 coord || f2 coord
 
 cut :: Shape -> Shape -> Shape
-cut = todo
+cut (Shape f1) (Shape f2) = Shape f
+  where
+    f :: Coord -> Bool
+    f coord = f1 coord && not (f2 coord)
 ------------------------------------------------------------------------------
 
 -- Here's a snowman, built using union from circles and rectangles.
@@ -277,7 +297,8 @@ exampleSnowman = fill white snowman
 --        ["000000","000000","000000"]]
 
 paintSolid :: Color -> Shape -> Picture -> Picture
-paintSolid color shape base = todo
+paintSolid color (Shape fs) (Picture base) = Picture f
+  where f coord = if fs coord then color else base coord
 ------------------------------------------------------------------------------
 
 allWhite :: Picture
@@ -322,7 +343,8 @@ stripes a b = Picture f
 --       ["000000","000000","000000","000000","000000"]]
 
 paint :: Picture -> Shape -> Picture -> Picture
-paint pat shape base = todo
+paint (Picture pat) (Shape fs) (Picture base) = Picture f
+  where f coord = if fs coord then pat coord else base coord
 ------------------------------------------------------------------------------
 
 -- Here's a patterned version of the snowman example. See it by running:
@@ -385,19 +407,21 @@ xy = Picture f
 data Fill = Fill Color
 
 instance Transform Fill where
-  apply = todo
+  apply (Fill c) (Picture f) = Picture (\coord -> c)
 
 data Zoom = Zoom Int
   deriving Show
 
 instance Transform Zoom where
-  apply = todo
+  apply (Zoom z) (Picture f) = Picture (\(Coord x y) -> f (Coord (div x z) (div y z)))
 
 data Flip = FlipX | FlipY | FlipXY
   deriving Show
 
 instance Transform Flip where
-  apply = todo
+  apply FlipX (Picture f) = Picture (\(Coord x y) -> f (Coord (-x) y))
+  apply FlipY (Picture f) = Picture (\(Coord x y) -> f (Coord x (-y)))
+  apply FlipXY (Picture f) = Picture (\(Coord x y) -> f (Coord y x))
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
@@ -411,9 +435,9 @@ instance Transform Flip where
 
 data Chain a b = Chain a b
   deriving Show
-
-instance Transform (Chain a b) where
-  apply = todo
+-- apply :: t -> Picture -> Picture
+instance (Transform a, Transform b) => Transform (Chain a b) where
+  apply (Chain a b) pic = apply a (apply b pic)
 ------------------------------------------------------------------------------
 
 -- Now we can redefine largeVerticalStripes using the above Transforms.
@@ -450,8 +474,21 @@ checkered = flipBlend largeVerticalStripes2
 data Blur = Blur
   deriving Show
 
+sumlist :: [Color] -> Color
+sumlist [] = Color 0 0 0
+sumlist ((Color r g b):cs) = 
+  case sumlist cs of
+    (Color sr sg sb) -> Color (sr + r) (sg + g) (sb + b)
+
+avglist :: [Color] -> Color
+avglist cs = case sumlist cs of
+    (Color sr sg sb) -> Color (div sr cl) (div sg cl) (div sb cl)
+    where cl = length cs
+
 instance Transform Blur where
-  apply = todo
+  apply Blur (Picture base) = Picture f
+    where 
+      f (Coord x y) = avglist [base (Coord x y), base (Coord (x + 1) y), base (Coord (x - 1) y), base (Coord x (y + 1)), base (Coord x (y - 1))]
 ------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------
@@ -469,7 +506,8 @@ data BlurMany = BlurMany Int
   deriving Show
 
 instance Transform BlurMany where
-  apply = todo
+  apply (BlurMany 0) pic = pic
+  apply (BlurMany n) pic = apply (BlurMany (n - 1)) (apply Blur pic)
 ------------------------------------------------------------------------------
 
 -- Here's a blurred version of our original snowman. See it by running
